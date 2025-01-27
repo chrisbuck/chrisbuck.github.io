4213768d6ffa47332200917664e389c38f67d8e5
ChrisBuck <chrisbuck29@gmail.com>
2025-01-26 22:48:21 -0500

    Fix navbar link directing to wrong page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.scss";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import TopNav from "@components/TopNav";
-import Home from "@pages/home/index";
+// import Home from "@pages/home/index";
+import Home from "@pages/brown/visual-storytelling/final-project/index";
 import { generateRoutes } from "./utils/generateRoutes";
 
 function App() {
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 import styles from "./TopNav.module.scss";
 
 const TopNav = () => {
-  return (
-    <header>
-        <Navbar expand="lg" className={`bg-body-tertiary ${styles.navbar}`}>
-        <Container fluid>
-          <Navbar.Brand href="https://chrisbuck.github.io/" className={styles.navbarBrand}>ChrisBuck.github.io</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+    return (
+        <header>
+            <Navbar expand="lg" className={`bg-body-tertiary ${styles.navbar}`}>
+                <Container fluid>
+                    <Navbar.Brand href="https://chrisbuck.github.io/#/" className={styles.navbarBrand}>
+                        ChrisBuck.github.io
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        {/* <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  );
-}
+            </Nav> */}
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
 
 export default TopNav;
